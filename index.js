fb9aa3cb55a4adbbbfb50339f4a0c1dfba620c9b
drissbejgana <93830096+drissbejgana@users.noreply.github.com>
2023-12-05 12:25:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -13,8 +12,8 @@ app.use(bodyParser.json());
 // Serve static files (HTML, CSS, JavaScript)
 app.use(express.static(__dirname + '/public'));
 
-// Set up a basic route for the chat page
 app.post('/', (req, res) => {
+
   console.log(req.body)
   res.send(JSON.stringify(req.body))
 
